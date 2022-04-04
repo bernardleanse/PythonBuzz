24731d5f3baef93ba2fba7aab47166dc3b17b541
Bernard Leanse <bernardleanse96@gmail.com>
2022-04-04 10:28:00 +0100

    all pass

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -1,5 +1,8 @@
 class FizzBuzz:
   def fizzbuzz(self, number):
+    if number % 3 == 0 and number % 5 == 0:
+      return "fizzbuzz" 
+  
     return "fizz" if number % 3 == 0 else "buzz"
 
   
--- fizz_buzz_test.py
+++ fizz_buzz_test.py
@@ -4,8 +4,14 @@ class TestFizzBuzz:
   def test_returns_fizz_if_divisible_by_three(self):
     fb = FizzBuzz()
     assert fb.fizzbuzz(3) == 'fizz'
+    assert fb.fizzbuzz(6) == 'fizz'
 
   def test_returns_buzz_if_divisble_by_five(self):
     fb = FizzBuzz()
     assert fb.fizzbuzz(5) == 'buzz'
+    assert fb.fizzbuzz(10) == 'buzz'
+
+  def test_returns_fizz_buzz_if_divsible_by_both(self):
+    fb = FizzBuzz()
+    assert fb.fizzbuzz(15) == 'fizzbuzz'
   
